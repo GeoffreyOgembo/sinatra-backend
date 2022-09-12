b86c796cf7c3405dbee4d9a4b6baace48ab22089
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-09-12 12:13:27 -0400

    environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,8 @@ configure :production do
   
     
 ActiveRecord::Base.establish_connection(  
-    :adapter => db.scheme == 'postgres' ? 'postgresql' 
-    :db.scheme,    
-    :host     => db.host,    
+    :adapter => db.scheme == 'postgres' ? 'postgresql':db.scheme,    
+    :host => db.host,    
     :username => db.user,   
     :password => db.password,    
     :database => db.path[1..-1],    
